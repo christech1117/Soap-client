0ff31f20d7ff013fe3726f5a1bbef66b1465d1df
username <user email>
2017-05-18 16:53:45 +0800

    去掉#，重設router起始位置

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,12 @@ import Hello from '@/components/Hello'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', // html5
+  base: __dirname, // router起始位置
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    { path: '/', name: 'Hello', component: Hello }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
