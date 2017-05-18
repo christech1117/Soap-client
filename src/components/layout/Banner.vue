8ae57a1760ec553ff0e1749b9211d1b740a0bef1
username <user email>
2017-05-18 18:05:36 +0800

    banner組件化

---
+++ src/components/layout/Banner.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #banner
+    .banner-img(:style="banner_css")
+</template>
+
+<script>
+export default {
+  props: ['banner_data'],
+  computed: {
+    banner_css () {
+      return {
+        'background-image': "url('" + this.banner_data + "')"
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.banner-img
+  background: no-repeat center
+  height: 300px
+  margin-top: 70px
+  background-size: cover
+</style>
---
+++ src/page/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #home
+    banner(:banner_data="banner_img")
+</template>
+
+<script>
+import Banner from '../components/layout/Banner.vue'
+
+export default {
+  components: { Banner },
+  data () {
+    return {
+      banner_img: 'http://demo.mor-e.com.tw/demo/yatia/images/main/6b149f1fdc0c0f8e2cc4594a60ec0b03.jpg'
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+</style>
