0c41aa91f1295b3a5b1907a33a234c1152e715f6
christech1117 <christech1117@gmail.com>
2017-05-25 15:59:42 +0800

    新增401, 404頁面

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -4,10 +4,11 @@
 </template>
 
 <script>
+import NavBar from '@/components/layout/NavBar.vue'
 import Banner from '@/components/layout/Banner'
 
 export default {
-  components: { Banner },
+  components: { NavBar, Banner },
   data () {
     return {
       banner_img: 'http://demo.mor-e.com.tw/demo/yatia/images/main/6b149f1fdc0c0f8e2cc4594a60ec0b03.jpg'
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/page/Home'
 import Product from '@/page/Product'
 import SingleProduct from '@/page/Product/SingleProduct'
@@ -9,20 +10,39 @@ import ForgetPass from '@/page/Account/ForgetPass'
 import About from '@/page/About'
 import Contact from '@/page/Contact'
 
+import Err404 from '../page/Error/404'
+import Err401 from '../page/Error/401'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history', // html5
   base: __dirname, // router起始位置
   routes: [
-    { path: '/', name: 'Home', component: Home },
+    // {
+    //   path: '/',
+    //   redirect: '/readme'
+    // },
+    { path: '/', component: Home },
     { path: '/productList', name: 'Product', component: Product },
     { path: '/productList/:id', name: 'SingleProduct', component: SingleProduct },
     { path: '/login', name: 'Login', component: Login },
     { path: '/register', name: 'Register', component: Register },
     { path: '/forgetPass', name: 'ForgetPass', component: ForgetPass },
     { path: '/about', name: 'About', component: About },
-    { path: '/contact', name: 'Contact', component: Contact }
+    { path: '/contact', name: 'Contact', component: Contact },
+    { path: '/401', component: Err401, name: '401' },
+    { path: '/404', component: Err404, name: '404' },
+    // {
+    //   path: '/errorpage',
+    //   name: '错误页面',
+    //   icon: '404',
+    //   children: [
+    //     { path: '401', component: Err401, name: '401' },
+    //     { path: '404', component: Err404, name: '404' }
+    //   ]
+    // },
+    { path: '*', redirect: '/404', hidden: true }
   ],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
